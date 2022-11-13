64e61608b2213b0380fc6a29a96d5dfc3e31d8a7
Oleksandr Zelenchuk <sasha.zelenchuk@gmail.com>
2022-11-13 22:55:26 +0200

    Update security group module version

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "4.16.0"
 
   name   = "blog"
   vpc_id = module.vpc.public_subnets[0]
